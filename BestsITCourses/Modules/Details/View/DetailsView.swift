151ca4c3dfce86a9503e8db6fb44e2bdf90bad1b
Guilherme Moraes <guilherme.moraes@ab-inbev.com>
2021-06-16 14:34:27 -0300

    Add Youtube Player in Details Class

---
+++ BestsITCourses/Modules/Details/View/DetailsView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsView.swift
+//  BestsITCourses
+//
+//  Created by Guilherme Moraes on 15/06/21.
+//
+
+import Foundation
---
+++ BestsITCourses/Modules/Details/ViewModel/DetailsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailsViewModel.swift
+//  BestsITCourses
+//
+//  Created by Guilherme Moraes on 15/06/21.
+//
+
+import Foundation
